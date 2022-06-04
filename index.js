e0b57f40202aae689d71cb89fe07bb7124eff281
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-04 16:41:52 -0400

    variables set

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var words = [
+ var words = [
   'bananas',
   'grapes',
   'carousel',
@@ -11,3 +11,19 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var game = document.getElementsByClassName('.game')
+var wordstoguess = document.getElementById('word-to-guess')
+var previousWord = document.getElementById('previous-word')
+var incorrectLetters = document.getElementById('incorrect-letters')
+var guessesRemaining = document.getElementById('remaining-guesses')
+var score = document.getElementsByClassName('.score')
+var wins = document.getElementById('wins')
+var losses = document.getElementById('losses')
+
+var correct = 0
+var incorrect = 0
+
+document.onkeyup = function(e) {
+  console.log(e.key)
+}
