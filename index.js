6d8724aeec5d20aea09267c53871e52c99e7778c
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-05 19:00:03 -0400

    I give up

--- index.js
+++ index.js
@@ -10,20 +10,47 @@
   'meatloaf',
   'ukulele',
   'mango'
-]
+];
+  
+var guessesLeft = 10; document.getElementById('remaining-guesses').innerHTML = guessesLeft;  
 
-var game = document.getElementsByClassName('.game')
-var wordstoguess = document.getElementById('word-to-guess')
 var previousWord = document.getElementById('previous-word')
-var incorrectLetters = document.getElementById('incorrect-letters')
-var guessesRemaining = document.getElementById('remaining-guesses')
-var score = document.getElementsByClassName('.score')
-var wins = document.getElementById('wins')
-var losses = document.getElementById('losses')
 
-var correct = 0
-var incorrect = 0
+var underScore = []; 
+ 
+var inncorrectLetters = Array.prototype.slice.call(document.getElementById('incorrect-letters'));
+ 
+var wins = document.getElementById('wins');
+var losses = document.getElementById('losses');
 
-document.onkeyup = function(e) {
-  console.log(e.key)
+var wins = 0
+var losses = 0
+
+document.onkeyup = function (e) {
+  // filter keypresses
+  var key = e.key.toLowerCase()
+  if (chosenWord.indexOf(key) == -1) return
+  // pick random word
+  var randNum = [Math.floor(Math.random() * words.length)];
+  var chosenWord = words[randNum]; document.getElementsById('word-to-guess');
+ }
+ // generate underscores based on the length of word
+ function generateUnderscore() {
+  for (let i = 0; i < chosenWord.length; i++) {
+    underScore.push('_');
+  }
+  return underScore;
 }
+// compare letter to word
+if (key === chosenWord) {
+    // increment and show correct/incorrect
+    wins++
+    wins.textContent = wins
+} else {
+    losses++
+    losses.textContent = losses
+  }
+//show previous word and letter
+previousWord.textContent = chosenWord
+inncorrectLetters.textContent = inncorrectLetters
+
