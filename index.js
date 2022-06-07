c75f281cadcd84e2220b03250bf733e99bf1e82d
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-06 20:24:13 -0400

    updated with final code

--- index.js
+++ index.js
@@ -12,113 +12,78 @@
   'mango'
 ];
   
-var currentWord = [];
-var answerWord = [];
-var pastLetters = [];
-var lettersReset = "";
-var i;
-var guessesLeft = 10;
+var wordToGuess = document.getElementById('word-to-guess')
+var remainingGuessDisplay = document.getElementById('remaining-guesses')
+var incorrectLetterDisplay = document.getElementById('incorrect-letters')
+var previousWord = document.getElementById('previous-word')
+var winDisplay = document.getElementById('wins')
+var lossDisplay = document.getElementById('losses')
 
-//math function to randomly pick a word from the wordbank
-var answer = words[Math.floor(Math.random() * words.length)];
+var wins = 0
+var losses = 0
+var remainingGuesses
+var correctLetters
+var incorrectLetters
+var currentWord
+var displayWord
 
-//variable that counts the number of guesses left
-document.getElementById("remaining-guesses").innerHTML = guessesLeft;
-
-//variable that counts the number of wins
-var wins = 0;
-document.getElementById("wins").innerHTML = wins;
-
-//variable that counts the number of losses
-var losses = 0;
-document.getElementById("losses").innerHTML = losses;
-
-//loop that creates empty spaces for the words
-for (i = 0; i < answer.length; i++) {
-  currentWord.push("_");
+// Create a new game for win, loss
+function newGame() {
+  currentWord = words[Math.floor(Math.random() * words.length)]
+  correctLetters = []
+  incorrectLetters = []
+  remainingGuesses = 10
+  updateGuessesInfo()
 }
-document.getElementById("word-to-guess").innerHTML = currentWord.join(" ");
 
-//evaluate the position of a letter in the word
-function wordLetters(letter) {
-  var letterPos = new Array();
-  for (i = 0; i < answer.length; i++) {
-    if (answer[i] === letter)
-      letterPos.push(i);
+function updateGuessesInfo() {
+  displayWord = ""
+
+  for(var i = 0; i < currentWord.length; i++){
+    if(correctLetters.indexOf(currentWord[i]) > -1){
+
+      console.log(currentWord[i])
+
+      displayWord += currentWord[i]
+    } else {
+      displayWord += '_'
+    }
   }
-  return letterPos;
+
+  remainingGuessDisplay.textContent = remainingGuesses
+  incorrectLetterDisplay.textContent = incorrectLetters.join(", ")
+  wordToGuess.textContent = displayWord
 }
 
-//return letters that arent guessed still
-function lettersToGuess() {
-  var i;
-  var toGuess = 0;
-  for (i in currentWord) {
-    if (currentWord[i] === "_")
-      toGuess++;
+//Create the key press functionality
+document.onkeyup = function(e){
+  var guessedLetter = e.key.toLowerCase()
+
+  if(!/^[a-z]{1}$/g.test(guessedLetter)) return
+
+  if(currentWord.includes(guessedLetter)){
+    correctLetters.push(guessedLetter)
+  } else if (!currentWord.includes(guessedLetter)){
+    incorrectLetters.push(guessedLetter)
+    
+    remainingGuesses--
   }
-  return toGuess;
-}
 
-//function to capture user input
-document.onkeyup = function(event) {
-  var letter = event.key.toLowerCase();
-  var lettersGuessed = letter;
-  var i;
-  var letterPos = wordLetters(lettersGuessed);
-}
+  updateGuessesInfo()
 
- //comparing letter guessed with the current word
- if (letterPos.length) {
-  for (i = 0; i < letterPos.length; i++) {
-    currentWord[letterPos[i]] = lettersGuessed;
+  if(displayWord === currentWord){
+    wins++
+    winDisplay.textContent = wins
+    previousWord.textContent = currentWord
+    newGame()
   }
-  document.getElementById("word-to-guess").innerHTML = currentWord.join(" ");
-} else {
-  //if letter has already been seen don't do it again
-  if (!pastLetters.includes(letter)) {
-    pastLetters.push(letter);
-    document.getElementById("incorrect-letters").innerHTML += lettersGuessed + " ";
-    guessesLeft--;
-    document.getElementById("remaining-guesses").innerHTML = guessesLeft;
-}
 
-//user correctly guesses word the game is reset
-if (lettersToGuess() == 0) {
-  guessesLeft = 10;
-  document.getElementById("remaining-guesses").innerHTML = guessesLeft;
-
-  document.getElementById("incorrect-letters").innerHTML = lettersReset;
-
-  answer = wordAnswers[Math.floor(Math.random() * wordAnswers.length)];
-
-  currentWord = [];
-  pastLetters = [];
-  for (i = 0; i < answer.length; i++) {
-    currentWord.push("_");
+  if(remainingGuesses <= 0){
+    losses++
+    lossDisplay.textContent = losses
+    previousWord.textContent = currentWord
+    newGame()
   }
-  document.getElementById("word-to-guess").innerHTML = currentWord.join(" ");
-
-  wins++;
-  document.getElementById("wins").innerHTML = wins;
 }
 
-//resets game if out of guesses
-if (guessesLeft === 0) {
-  guessesLeft = 10;
-  document.getElementById("remaining-guesses").innerHTML = guessesLeft;
-  document.getElementById("incorrect-letters").innerHTML = lettersReset;
-
-  answer = words[Math.floor(Math.random() * words.length)];
-
-  currentWord = [];
-  pastLetters = [];
-  for (i = 0; i < answer.length; i++) {
-    currentWord.push("_");
-  }
-  document.getElementById("word-to-guess").innerHTML = currentWord.join(" ");
-
-  losses++;
-  document.getElementById("losses").innerHTML = wins;
-}
-}
+newGame()
